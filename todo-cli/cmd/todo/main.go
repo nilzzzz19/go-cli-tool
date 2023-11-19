f5ebce4bbed4ebb97d1ebd0f4d7db3d150c60d88
Nilay Nagar <45895008+nilzzzz19@users.noreply.github.com>
2023-11-19 05:47:01 +0000

    updated todo and main

--- todo-cli/cmd/todo/main.go
+++ todo-cli/cmd/todo/main.go
@@ -1,9 +1,41 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"os"
+
+	todo "github.com/nilzzzz19/go-cli-tool"
+)
+
+const (
+	todoFile = ".todos.json"
+)
 
 func main() {
+	//flags
+	add := flag.Bool("add", false, "Add a todo item") //returns the address of the bool var
 
-	fmt.Println("Hello Go!")
+	//parsing in cli
+	flag.Parse()
 
+	todos := &todo.Todos{} //gets the ds
+
+	if err := todos.Load(todoFile); err != nil {
+		fmt.Fprintln(os.Stderr, err.Error())
+		os.Exit(1)
+	}
+
+	switch {
+	case *add:
+		todos.Add("sample task")
+		err := todos.Store(todoFile)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err.Error())
+			os.Exit(1)
+		}
+	default:
+		fmt.Fprintln(os.Stderr)
+		os.Exit(1)
+	}
 }
--- todo-cli/todo.go
+++ todo-cli/todo.go
@@ -1,7 +1,10 @@
 package todo
 
 import (
+	"encoding/json"
 	"errors"
+	"fmt"
+	"os"
 	"time"
 )
 
@@ -38,4 +41,34 @@ func (t *Todos) Delete(index int) error {
 	*t = append(ls[:index-1], ls[index:]...)
 
 	return nil
+}
+
+func (t *Todos) Load(filename string) error {
+	file, err := os.ReadFile(filename)
+	if err != nil {
+		if errors.Is(err, os.ErrNotExist) {
+			return nil
+		}
+		return err
+	}
+	err = json.Unmarshal(file, t) //cant redeclare local scope var
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (t *Todos) Store(filename string) error {
+	data, err := json.Marshal(t)
+	if err != nil {
+		return err
+	}
+	return os.WriteFile(filename, data, 0777) //0644 is the os permission
+}
+
+func (t *Todos) Print() {
+	for i, item := range *t {
+		fmt.Print(i, item.Task)
+	}
 }
